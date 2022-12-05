f073414bc4bb72906bd9fb2b099711d7e80576bd
Raissa <raissaolivfac@gmail.com>
2022-12-04 21:05:34 -0300

    Criação da Classe C

---
+++ ClasseC.java
@@ -0,0 +1,13 @@
+public class ClasseC {
+    private String C1;
+    private int C2;
+ 
+    public void MC1(){
+        System.out.println("MC1");
+    }
+
+    public void MC2(){
+        System.out.println("MC2");
+    }
+
+}
